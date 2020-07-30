0eb76a29e690a08848cc574ca032d098591adf9b
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2020-07-30 15:04:15 -0700

    Changed examples to yarn, updated versioning to use package.json

--- internal/unavailable.js
+++ internal/unavailable.js
@@ -2,5 +2,5 @@ const chalk = require('chalk');
 
 console.log()
 console.log(`${chalk.red('Error: ')} This command is not available in development.`)
-console.log(`Run ${chalk.cyan('npm run generate')} and follow the instructions.`)
+console.log(`Run ${chalk.cyan('yarn generate')} and follow the instructions.`)
 console.log()
