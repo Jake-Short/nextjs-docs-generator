3a228e33edb1bfcd1f07c2795a6e4d6aacf423ee
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2020-07-30 13:12:17 -0700

    Added version checking to upgrade and command

--- internal/upgrade.js
+++ internal/upgrade.js
@@ -5,62 +5,65 @@ const fse = require('fs-extra');
 const path = require('path');
 const download = require('download-git-repo');
 
+/** let jsonData = JSON.parse(fs.readFileSync(parent + '/package.json', 'utf-8')); */
+
 console.log('');
-inquirer.prompt([
-	{
-		type: 'checkbox',
-		name: 'Folders to update',
-		message: () => console.log(`${chalk.cyan.bold('Update type: ')} What folders would you like to update?`),
-		choices: ['internal', 'styles', 'components', 'pages']
-	}
-])
-.then(answers => {
+console.log(`${chalk.blue('Downloading repository...')}`);
+console.log('');
+
+const internalDir = path.resolve(__dirname);
+download('Jake-Short/nextjs-docs-generator#main', `${internalDir}/.temp`, (error) => {
+	let jsonData = JSON.parse(fs.readFileSync(`${internalDir}/.temp/internal/info.json`, 'utf-8'));
+	let version = jsonData.version;
+	let currVersion = JSON.parse(fs.readFileSync(`${internalDir}/info.json`, 'utf-8')).version;
+
 	console.log('');
-	const foldersToUpdate = answers['Folders to update'];
-	const consoleLogString = `${chalk.red.bold('Warning: ')} This will ${chalk.bold('clear')} the contents of these folders: ${chalk.cyan(`${foldersToUpdate.join(', ')}`)}.`
-	
-	if(foldersToUpdate.length < 1) {
-		console.log(`${chalk.cyan(`You didn't select any files!`)} Aborting update.`);
+	console.log(`You are upgrading from version ${chalk.cyan.bold(currVersion)} to ${chalk.cyan.bold(version)}.`);
+	console.log('');
+
+	if(error) {
+		console.log(`${chalk.red.bold('Error: ')} There was a problem downloading the repository. (${error})`);
+		console.log(`Update aborted.`);
 		console.log('');
-		return;
 	}
-
-	inquirer.prompt([
-		{
-			type: 'confirm',
-			name: 'Continue',
-			message: () => console.log(consoleLogString),
-			default: false
-		}
-	])
-	.then(res => {
-		if(res.Continue === true) {
-			// Trigger update
-			if(res.Continue === true) {
-				const internalDir = path.resolve(__dirname);
-				const baseDir = path.resolve(__dirname, '..');
-
-				if(!fs.existsSync(internalDir + '/.temp')) {
-					fs.mkdirSync(internalDir + '/.temp');
+	else {
+		inquirer.prompt([
+			{
+				type: 'checkbox',
+				name: 'Folders to update',
+				message: () => console.log(`${chalk.cyan.bold('Update type: ')} What folders would you like to update?`),
+				choices: ['internal', 'styles', 'components', 'pages']
+			}
+		])
+		.then(answers => {
+			console.log('');
+			const foldersToUpdate = answers['Folders to update'];
+			const consoleLogString = `${chalk.red.bold('Warning: ')} This will ${chalk.bold('clear')} the contents of these folders: ${chalk.cyan(`${foldersToUpdate.join(', ')}`)}.`
+			
+			if(foldersToUpdate.length < 1) {
+				console.log(`${chalk.cyan(`You didn't select any files!`)} Aborting update.`);
+				console.log('');
+				return;
+			}
+		
+			inquirer.prompt([
+				{
+					type: 'confirm',
+					name: 'Continue',
+					message: () => console.log(consoleLogString),
+					default: false
 				}
-
-				console.log('');
-				console.log(`${chalk.blue('Downloading repository...')}`);
-				console.log('');
-
-				
-				download('Jake-Short/nextjs-docs-generator#main', `${internalDir}/.temp`, (error) => {
-					if(error) {
-						console.log(`${chalk.red.bold('Error: ')} There was a problem downloading the repository. (${error})`);
-						console.log(`Update aborted.`);
-						console.log('');
-					}
-					else {
-						console.log(`${chalk.blue('Repository downloaded!')}`);
-						console.log('');
-						console.log(`${chalk.blue('Writing files...')}`);
-						console.log('');
-
+			])
+			.then(res => {
+				if(res.Continue === true) {
+					// Trigger update
+					if(res.Continue === true) {
+						const baseDir = path.resolve(__dirname, '..');
+		
+						if(!fs.existsSync(internalDir + '/.temp')) {
+							fs.mkdirSync(internalDir + '/.temp');
+						}
+		
 						// Copy selected folders below
 						if(foldersToUpdate.includes('internal')) {
 							fse.copySync(`${internalDir}/.temp/internal`, `${baseDir}/internal`);
@@ -84,20 +87,20 @@ inquirer.prompt([
 						console.log(`${chalk.green.bold('Success!')} Your project has been updated.`);
 						console.log('');
 					}
-				})
+				}
+			})
+			.catch(error => {
+				console.log(`${chalk.red.bold('Error: ')} An error occured. (${error})`)
+			});
+		})
+		.catch(error => {
+			if(error.isTtyError) {
+				// Prompt couldn't be rendered in the current environment
+				console.log(`${chalk.red.bold('Error: ')} The current environment is unsupported.`)
+			} else {
+				// Something else when wrong
+				console.log(`${chalk.red.bold('Error: ')} An error occured. (${error})`)
 			}
-		}
-	})
-	.catch(error => {
-		console.log(`${chalk.red.bold('Error: ')} An error occured. (${error})`)
-	});
+		});
+	}
 })
-.catch(error => {
-    if(error.isTtyError) {
-		// Prompt couldn't be rendered in the current environment
-		console.log(`${chalk.red.bold('Error: ')} The current environment is unsupported.`)
-    } else {
-		// Something else when wrong
-		console.log(`${chalk.red.bold('Error: ')} An error occured. (${error})`)
-    }
-});
